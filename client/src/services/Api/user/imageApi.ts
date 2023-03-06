d08b694537ed11dd885ed8f69c1ea996d0470b0c
Suryajith32 <suryajithms32@gmail.com>
2023-03-06 15:34:10 +0530

    changed imageapi

--- client/src/services/Api/user/imageApi.ts
+++ client/src/services/Api/user/imageApi.ts
@@ -1,5 +1,5 @@
-// const postsImages = "http://localhost:4000/api/images"
-const postsImages = "https://go-socialize.onrender.com/api/images"
+const postsImages = "http://localhost:4000/api/images"
+// const postsImages = "https://go-socialize.onrender.com/api/images"
 
 
 export default postsImages
